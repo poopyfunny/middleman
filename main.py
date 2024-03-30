35b5b64a0bb24d9a649e3fa7040e6285526d44e1
max <leonardoretardo420@gmail.com>
2024-03-30 14:25:19 +0300

    test

---
+++ main.py
@@ -0,0 +1,12 @@
+from tronpy import Tron
+from tronpy.keys import PrivateKey
+from tronpy.providers import HTTPProvider
+import sys
+
+
+print("awaiting input\n")
+
+text = input("")  
+
+text = str.strip(text)
+print (str.upper(text))
